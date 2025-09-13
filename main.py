638262733b5370baecf64809ff8187cc654453c6
Andro0129 <jorjo0129@gmail.com>
2025-09-13 11:34:07 +0200

    első módosítás

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#Ez az első módosítás AB
+#Ez az első módosítás,  by AB
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
